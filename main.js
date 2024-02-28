204a7f9cb603cc54227e3579dc766ce8dbd9acb0
Vinay sen <145519958+Vinnu7898@users.noreply.github.com>
2024-02-28 12:35:32 +0530

    Upload file

---
+++ main.js
@@ -0,0 +1,9 @@
+function visi() {
+  const a = document.querySelector(".sec");
+  const sec1 =document.querySelector("#sec1") ;
+  if (sec1.style.display!="none") {
+    sec1.style.display ="none";
+  } else {
+    sec1.style.display="block";
+  }
+}
